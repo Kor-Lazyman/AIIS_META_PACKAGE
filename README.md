1511e0920af44d545103e59fdc1e28e551936c5d
Kor-Lazyman <taekwonv0310@gmail.com>
2025-10-14 16:34:09 +0900

    main

---
+++ README.md
@@ -0,0 +1,10 @@
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118</br>
+pip install scipy</br>
+pip install simpy</br>
+pip install gym</br>
+pip install gymnasium</br>
+pip install pandas</br>
+pip install matplotlib</br>
+## FlowChart
+
+<img width="852" height="692" alt="meta-package_without_envs drawio (3)" src="https://github.com/user-attachments/assets/67d7ef9e-b339-4bb9-a1f6-a9fb6620019e" />
---
+++ main.py
@@ -0,0 +1,53 @@
+import numpy as np
+from envs.config_SimPy import *
+from envs.promp_env import MetaEnv
+from AIIS_META.Agents.Gaussian.Meta_Gaussian import MetaGaussianAgent
+from AIIS_META.Baselines.linear_baseline import LinearFeatureBaseline
+from AIIS_META.Algos.MAML.promp import ProMP
+from AIIS_META.Agents.Simple_Mlp import SimpleMLP
+import torch
+import torch.optim as optim
+from envs.config_folders import *
+def main(params):
+
+    env = MetaEnv()
+    mlp = SimpleMLP(np.prod(env.observation_space.shape), 
+                       np.prod(env.action_space.shape),
+                    hidden_layers = params["Layers"])
+    
+    agent = MetaGaussianAgent(mlp = mlp, num_tasks=params["num_task"], optimizer=optim.Adam(mlp.parameters(), lr = params["alpha"]))
+    
+    meta_algo = ProMP(env = env, max_path_length = params["max_path_length"],
+                    agent = agent, optimizer=optim.Adam(mlp.parameters(), lr = params["beta"]),
+                    baseline=LinearFeatureBaseline(),
+                    tensor_log=params["tensor_log"],
+                    inner_grad_steps= params["num_inner_grad"],
+                    num_tasks=params["num_task"], 
+                    outer_iters=params["outer_iters"], 
+                    parallel=params["parallel"], 
+                    rollout_per_task=params["rollout_per_task"], 
+                    clip_eps=params["num_task"], 
+                    device=params["device"])
+    
+    meta_algo.learn(params["epochs"])
+    torch.save(meta_algo.state_dict(), f"{SAVED_MODEL_PATH}/saved_model")
+if __name__ == "__main__":
+    params = {
+        "Layers":[64, 64, 64], # layers of Network
+        "rollout_per_task": 20,
+        "num_task": 5, # Number of tasks
+        "max_path_length": SIM_TIME,
+        "tensor_log": TENSORFLOW_LOGS,
+        "alpha": 0.002,
+        "beta": 0.0005,
+        "outer_iters": 5, # number of ProMp steps without re-sampling
+        "clip_eps": 0.3, # clip range for ProMP(outer) update
+        "num_inner_grad": 1,
+        "epochs": 500,
+        "discount": 0.99,
+        "gae_lambda": 1,
+        "parallel": True,
+        "device":torch.device("cpu")
+    }
+    main(params)
+   
